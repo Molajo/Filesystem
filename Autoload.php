e97a36c8b36685e5702e95cf25bb823dfc9bb473
AmyStephen <AmyStephen@gmail.com>
2013-02-09 01:52:56 -0600

    x

--- Autoload.php
+++ Autoload.php
@@ -24,3 +24,15 @@ $loader->add('Molajo', BASE_FOLDER . '/src/');
 $loader->add('Integration', BASE_FOLDER . '/tests/');
 
 $loader->register();
+
+spl_autoload_register(function($class) {
+        if (0 === (strpos($class, 'Molajo\\'))) {
+            $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 3)).'.php';
+
+            if (!stream_resolve_include_path($path)) {
+                return false;
+            }
+            require_once $path;
+            return true;
+        }
+    });
