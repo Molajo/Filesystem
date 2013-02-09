1f28396226f857825f01713c6a3aa3695e844389
AmyStephen <AmyStephen@gmail.com>
2013-02-09 10:55:42 -0600

    Why arent my testing running?

--- Autoload.php
+++ Autoload.php
@@ -17,7 +17,7 @@ if (substr($_SERVER['DOCUMENT_ROOT'], - 1) == '/') {
 define('BASE_FOLDER', __DIR__);
 
 $classMap = array(
-    'Molajo\\Filesystem\\Adapter'     => BASE_FOLDER . '/src/Molajo/Filesystem/Adapter.php',
+    'Molajo\\Filesystem\\Adapter'                                 => BASE_FOLDER . '/src/Molajo/Filesystem/Adapter.php',
     'Molajo\\Filesystem\\Adapter\\FilesystemActionsInterface'     => BASE_FOLDER . '/src/Molajo/Filesystem/Adapter/FilesystemActionsInterface.php',
     'Molajo\\Filesystem\\Adapter\\FilesystemInterface'            => BASE_FOLDER . '/src/Molajo/Filesystem/Adapter/FilesystemInterface.php',
     'Molajo\\Filesystem\\Adapter\\Fileupload'                     => BASE_FOLDER . '/src/Molajo/Filesystem/Adapter/Fileupload.php',
@@ -43,6 +43,7 @@ spl_autoload_register(
         }
     }
 );
+
 /*
 include BASE_FOLDER . '/' . 'ClassLoader.php';
 $loader = new ClassLoader();
